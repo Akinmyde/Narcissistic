d02810122b254ff126eca8a64bf4f0586606cbc6
Akinmyde <akinremiolumide96@gmail.com>
2018-10-17 13:28:16 +0100

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // use javascript only when the DOM element has completely loaded
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     
     // calculate any user input using the narcissistic formular
     const narcissistic = (value) => {
@@ -21,29 +21,29 @@ document.addEventListener("DOMContentLoaded", () => {
     // detect if the input is a narcissistic number or not
     const detectNarcissistic = () => {
         let input = document.getElementById('input').value;
-        if (input !== "" && !(input < 0) ) {
+        if (input !== '' && !(input < 0) ) {
             input = parseInt(input);
             let result = narcissistic(input);
             let msg;
             if (input === result) {
                 playAgain();
-                msg = "You should be proud of yourself, you can call yourself an ACE... your input is " + input + " and your result is " + result + " NARCISSISTIC EXPERT!!!"; 
+                msg = `You should be proud of yourself, you can call yourself an ACE... your input is  ${input} and your result is ${result} + " NARCISSISTIC EXPERT!!!`; 
                 return computerInputDisplay(msg,1000,"computer");
             } else {
                 playAgain();
-                msg = "lol, you can't even guess a Narcissistic Number...your input is " + input + " and your result is " + result;
-                return computerInputDisplay(msg,1000,"computererror");
+                msg = `lol, you can't even guess a Narcissistic Number...your input is ${input} and your result is ${result}`;
+                return computerInputDisplay(msg,1000,'computererror');
             }
         } else {
             playAgain();
-            msg = "you need to enter a number, it looks like we are having an empty value or a negative number";
-            return computerInputDisplay(msg,1000,"computererror");
+            msg = 'you need to enter a number, it looks like we are having an empty value or a negative number';
+            return computerInputDisplay(msg,1000,'computererror');
         }
     }
 
     //repeat the game after each result
     const playAgain = () => {
-        let play = document.getElementById('detect').innerHTML = "Play Again";
+        let play = document.getElementById('detect').innerHTML = 'Detect Narcissistic';
         clearNode();
     }
 
@@ -55,15 +55,18 @@ document.addEventListener("DOMContentLoaded", () => {
         computerMsg.setAttribute('class',idclass);
 
         setTimeout(() => {
-            document.getElementById("msg").appendChild(computerMsg);
+            document.getElementById('msg').appendChild(computerMsg);
         },delay);
     }
 
-    const openForm = () => {
-        document.getElementById('myForm').style.display = "block";
-        computerInputDisplay("Welcome! enter a number to play",1000,"computer");
+    //start the gane
+    const startGame = () => {
+        document.getElementById('myForm').style.display = 'block';
+        computerInputDisplay('Welcome! enter a number to play',1000,'computer');
+
         disableEnableButton('example');
         disableEnableButton('start');
+        //console.log(disableEnableButton('cancel'));
     };
 
     const disableEnableButton = (id) => {
@@ -75,18 +78,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
     
-    //open the narsissitic detector form
     const closeForm = () => {
         let computerMsg = document.createElement('div');
-        computerMsg.textContent = "Thanks for using the App!!!";
-        computerMsg.setAttribute('id',"computer");
-        computerMsg.setAttribute('class',"computer");
-        document.getElementById("msg").appendChild(computerMsg);
+        computerMsg.textContent = 'Thanks for using the App!!!';
+        computerMsg.setAttribute('id','computer');
+        computerMsg.setAttribute('class','computer');
+        document.getElementById('msg').appendChild(computerMsg);
         setTimeout(() => {
             clearText();
-            document.getElementById('myForm').style.display = "none";
+            document.getElementById('myForm').style.display = 'none';
             clearNode();
-        },2000)
+        },400)
 
         disableEnableButton('start');
         disableEnableButton('example');
@@ -99,17 +101,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //remove all message my the computer
     const clearNode = () => {
-        let msg = document.getElementById("msg");
+        let msg = document.getElementById('msg');
         while(msg.firstChild) {
             msg.removeChild(msg.firstChild);
         }
     }
 
     //shoews the user an example before playing the game
-    const examples = () => document.getElementById('alert').style.display = "block";
+    const examples = () => document.getElementById('alert').style.display = 'block';
 
     // close the example displayed
-    const closeExample = () => setTimeout(() => { document.getElementById('alert').style.display = "none"; }, 600);
+    const closeExample = () => setTimeout(() => { document.getElementById('alert').style.display = 'none'; }, 600);
 
 
     let start = document.getElementById('start');
@@ -117,11 +119,10 @@ document.addEventListener("DOMContentLoaded", () => {
     let detect = document.getElementById('detect');
     let input = document.getElementById('input');
     let example = document.getElementById('example');
-    let close = document.getElementById("closebtn");
+    let close = document.getElementById('closebtn');
 
-    
     // eventListners
-    start.addEventListener('click', openForm);
+    start.addEventListener('click', startGame);
     cancel.addEventListener('click', closeForm);
     detect.addEventListener('click', detectNarcissistic);
     input.addEventListener('focus',clearText);
